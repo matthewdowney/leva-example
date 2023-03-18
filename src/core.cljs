948a78a301a1aeaef4cda7c8ec2d312ff9df81a1
Matthew Downey <matthewdowney20@gmail.com>
2023-03-18 19:42:21 -0400

    Updated version which does indeed work

--- src/core.cljs
+++ src/core.cljs
@@ -1,17 +1,37 @@
 (ns core
-  (:require [leva.core :as leva]
+  (:require [leva :as leva-js]
+            ["react" :as react]
+            [leva.core :as leva-cljs]
+            [leva.schema :as schema]
             [reagent.core :as r]
             [reagent.dom :as rdom]))
 
 (defonce controls (r/atom {:width 500 :height 500}))
 
+(defn Controls* [opts]
+  (let [[watch-id] (react/useState (str (random-uuid)))
+        !state     (:atom opts)
+        initial    (if !state (.-state !state) {})
+        ks         (keys initial)
+        opts       (update opts :store #(or % (leva-js/useStoreContext)))
+        [_ set] (apply leva-js/useControls (schema/opts->argv opts))]
+    (react/useEffect
+      (fn mount []
+        (if !state
+          (do
+            (add-watch !state watch-id
+              (fn [_ _ _ new-state]
+                (set (clj->js (select-keys new-state ks)))))
+            (fn unmount []
+              (remove-watch !state watch-id)))
+          js/undefined)))
+    nil))
+
 (defn app []
-  [:div
-   [leva/SubPanel {:fill true :titleBar {:drag false}}
-    [leva/Controls {:folder {:name "Plot Controls"} :atom controls}]]])
+  [:f> leva-cljs/SubPanel {:fill true :titleBar {:drag false}}
+   [:f> Controls* {:folder {:name "Plot Controls"} :atom controls}]])
 
 ;;; Lifecycle / entry point
 
 (defn start [] (rdom/render [app] (.getElementById js/document "app")))
-(defn stop [] (js/console.log "stopping..."))
 (defn ^:export init [] (start))
